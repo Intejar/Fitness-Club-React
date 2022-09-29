6dd1359a25b83b7f85bb581cd0b0451cc131a2f7
Intejar <meherabintejar@gmail.com>
2022-09-29 21:59:28 +0600

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Product.css'
+const Product = (props) => {
+    const { name, info, age, time, img } = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" ></img>
+            <div className="product-info">
+                <p>Name:{name}</p>
+                <p>{info}</p>
+                <p>For Age: {age}</p>
+                <p>Time required: {time}</p>
+            </div>
+            <button>add</button>
+        </div>
+
+    );
+};
+
+export default Product;
